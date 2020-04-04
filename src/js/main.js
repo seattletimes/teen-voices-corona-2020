e400e00bc55426ad14f4cdda70b3831b8ec3c158
llflannery <lflannery@seattletimes.com>
2020-04-03 19:34:03 -0700

    add padding for iframe

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,6 @@
 // setTimeout(() => paywall(12345678), 5000);
 
 require("component-responsive-frame/child");
-
 var $ = require('jquery');
 
 
@@ -11,60 +10,44 @@ $( ".box" ).click(function() {
 
   $('.container').find(`.overlay`).css("opacity",0);
 
-  $(".box").css("background-color","white");
-  $(this).css("background-color","orange");
+  $('.container').find(`.story[data-number='${storyNum}']`).css("display","block");
 
-  if($('.story').is(':visible')) {
-    $('.story').slideUp(100);
-    $('.container').find(`.story[data-number='${storyNum}']`).delay(200).queue(function (next) {
-    $(this).slideDown();
-    next();
-  });
-  } else {
-    $('.container').find(`.story[data-number='${storyNum}']`).slideDown();
-  }
-
-
-  console.log(storyNum);
-
+  $(".box").css("opacity",0.7);
   clearTimeout(timer);
 
 });
 
+$( ".fa-times-circle" ).click(function() {
+  $(this).parent(".story").css("display","none");
+  $(".box").css("opacity",1);
+  loop();
+});
+
 var timer;
 
-
-// $(this).data("id");
 var randomArray = [5, 2, 8, 1, 6, 3, 7, 4, 9];
-
 var start = 0;
 
-$( document ).ready(function() {
 
-  (function loop () {
-    // console.log(start);
+
+
+  function loop() {
 
     $('.container').find(`.overlay[data-number='${randomArray[start]}']`).css("opacity",1);
-
-    // console.log("hi mom");
     $('.container').find(`.overlay[data-number='${randomArray[start]}']`).delay(4000).queue(function (next) {
     $(this).css("opacity",0);
     next();
   });
 
-    // var ranNum = Math.random() * (4 - 1) + 1;
-
-    // console.log(ranNum);
     start = (start === 8) ? 0 : start + 1;
-
-
-
     timer = window.setTimeout(loop, 5000);
 
 
+  }
 
 
+$( document ).ready(function() {
 
-  })();
+  loop();
 
 });
